555bcfe9f93200e3303648f33d2b844f4c03e2df
M. Attique <142322826+mr-attique-developer@users.noreply.github.com>
2025-01-13 18:57:11 +0500

    Time: 0 ms (100%), Space: 17.7 MB (21.89%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,7 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        integer = int("".join(map(str,digits)))
+
+        integer +=1
+        return [int(i) for i in str(integer)]
+    
