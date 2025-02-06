9c608b54053cadda511971cafbc09222dd7f2a21
M. Attique <142322826+mr-attique-developer@users.noreply.github.com>
2025-02-06 14:00:53 +0500

    Time: 5 ms (67.07%), Space: 18.6 MB (68.97%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -0,0 +1,18 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        
+        total_sum = sum(nums)
+        left_sum = 0
+
+        for i in range(len(nums)):
+
+            right_sum = total_sum - left_sum - nums[i]
+
+            if right_sum == left_sum:
+                return i
+            else:
+                left_sum += nums[i]
+        return -1
+
+
+            
