5388bd76462c293df6f400d1ee0ac5e8b7d643dc
M. Attique <142322826+mr-attique-developer@users.noreply.github.com>
2025-03-27 23:48:22 +0500

    Time: 2 ms (57.42%), Space: 64.3 MB (44.2%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -4,19 +4,14 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    let n = nums.length;
-    k = k % n; // Handle cases where k > n
-
-    const reverse = (arr, start, end) => {
-        while (start < end) {
-            [arr[start], arr[end]] = [arr[end], arr[start]];
-            start++;
-            end--;
-        }
-    };
-
-    reverse(nums, 0, n - 1); // Reverse the entire array
-    reverse(nums, 0, k - 1); // Reverse the first k elements
-    reverse(nums, k, n - 1); // Reverse the remaining elements
+    let n = nums.length
+    k = k % n;
+    temp = new Array(n)
+  for(let i = 0; i< n; i++){
+    temp[(i+k)% n] = nums[i]
+  } 
+  for(let i = 0; i< n; i++){
+    nums[i] = temp[i]
+  } 
 };
 
